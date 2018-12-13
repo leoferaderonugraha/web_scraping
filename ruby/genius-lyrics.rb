5f5f5de04d73064a5120251a28447183a613adba
Leo Feradero Nugraha <leoferaderonugraha@yahoo.com>
2018-12-13 22:30:02 +0700

    Update genius-lyrics.rb
    remove unnecessary stuff

--- ruby/genius-lyrics.rb
+++ ruby/genius-lyrics.rb
@@ -5,19 +5,21 @@ require 'open-uri'
 require 'mechanize'
 
 if ARGV.empty?
-  puts "Usage #{__FILE__} \"artis - songs title\""
-  exit
+    puts "Usage #{__FILE__} \"artis - songs title\""
+    exit
 end
 
 uri = "https://www.bing.com/search?q=#{ARGV.join('+')}+site:genius.com"
 links = []
+data = []
 mech = Mechanize.new
 mech.redirect_ok = true
 mech.user_agent = 'Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1'
 
 mech.get(uri)
 doc = Nokogiri::HTML(mech.page.body)
-doc.css('li.b_algo a').map{|link| links.append(link['href'])}
+doc.css('li.b_algo a').each{|link| links.append(link['href'])}
+doc = Nokogiri::HTML(open(links[0]).read) #open the first link
+doc.css('p').each {|x| data << x.content} #extract data between elements
 
-doc = Nokogiri::HTML(open(links[0]).read)
-doc.css('p').each {|data| puts data.text}
+puts data[0]
